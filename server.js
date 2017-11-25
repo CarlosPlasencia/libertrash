0fd61a9cc4ec799c5a6f1df5ed671d1a0558f481
CarlosPlasencia <plasenciacarlos7@gmail.com>
2017-11-25 16:23:48 -0500

    subir de nuevo 2

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ mongoose.Promise = global.Promise;
 // INICIAR SERVIDOR
 // Se corre el servidor en el puerto 8000
 server.listen(8000, function () {
-    console.log('Servidor esta escuchando en el puerto ' + 8000)
+    console.log('Servidor esta escuchando en el puerto ' + 5000)
 });
